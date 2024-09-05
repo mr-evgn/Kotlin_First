ef5a54593d995dfb7cf3fb9d04124d5a2197d9d9
Evgeny <evgeniievgeny@gmail.com>
2024-09-05 13:45:24 +0300

    second lesson

---
+++ src/main/kotlin/Home_Work2.kt
@@ -0,0 +1,45 @@
+package org.example
+
+fun main(args: Array<String>) {
+    val options = arrayOf("Rock", "Paper", "Scissors")
+    val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
+    printResult(userChoice, gameChoice)
+}
+
+fun getGameChoice(optionsParam: Array<String>) =
+    optionsParam[(Math.random() * optionsParam.size).toInt()]
+
+fun getUserChoice(optionsParam: Array<String>): String {
+    var isValidChoice = false
+    var userChoice = ""
+    while (!isValidChoice) {
+
+        print("Please enter one of the following:")
+        for (item in optionsParam) print(" $item")
+        println(".")
+
+        val userInput = readlnOrNull()
+
+        if (userInput != null && userInput in optionsParam) {
+            isValidChoice = true
+            userChoice = userInput
+        }
+
+        if (!isValidChoice) println("You must enter a valid choice.")
+    }
+    return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String) {
+    val result: String
+//Определить результат
+    if (userChoice == gameChoice) result = "Tie!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+        (userChoice == "Paper" && gameChoice == "Rock") ||
+        (userChoice == "Scissors" && gameChoice == "Paper")
+    ) result = "You win!"
+    else result = "You lose!"
+//Вывести результат
+    println("You chose $userChoice. I chose $gameChoice. $result")
+}
