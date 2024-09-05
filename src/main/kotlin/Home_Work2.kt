06dc431bb7cf83193986aff7bb64ffd69f51f1a2
Evgeny <evgeniievgeny@gmail.com>
2024-09-05 13:46:57 +0300

    second lesson

--- src/main/kotlin/Home_Work2.kt
+++ src/main/kotlin/Home_Work2.kt
@@ -33,13 +33,11 @@ fun getUserChoice(optionsParam: Array<String>): String {
 
 fun printResult(userChoice: String, gameChoice: String) {
     val result: String
-//Определить результат
     if (userChoice == gameChoice) result = "Tie!"
     else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
         (userChoice == "Paper" && gameChoice == "Rock") ||
         (userChoice == "Scissors" && gameChoice == "Paper")
     ) result = "You win!"
     else result = "You lose!"
-//Вывести результат
     println("You chose $userChoice. I chose $gameChoice. $result")
 }
